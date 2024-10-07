9a267e7bfb952d26a8d63bddea3813f5b7711c15
juniorCelis96 <jr.main.396@gmail.com>
2024-10-07 13:28:34 -0500

    -JC: Fix- Ajustes y documentación

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,6 @@ from .views import list_tasks, create_task, edit_task, delete_task
 urlpatterns = [
     path('', list_tasks),
     path('new/', create_task, name = 'create_task'),
-    path('/<int:task_id>/edit/', edit_task, name='edit_task'),
-    path('/<int:task_id>/delete/', delete_task , name='delete_task')
+    path('/<int:task_id>/edit', edit_task, name='edit_task'),
+    path('/<int:task_id>/delete', delete_task , name='delete_task')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 # Create your views here.
+"""summary: This Method permit obtain the tasks list
+    params(Type): request(Object): Body of the request
+"""
 def list_tasks(request):
     tasks = Task.objects.all()
     return render(request, 'list_tasks.html', {"tasks": tasks})
 
+"""summary: This Method permit create new tasks
+    params(Type): request(Object): Body of the request to storage
+"""
 def create_task(request):
     print("test: ", request.POST)
     
@@ -21,12 +27,20 @@ def create_task(request):
     task.save()
     return redirect('/tasks/')
 
+"""summary: This Method permit Delete the task
+    params(Type): request(Object): Body of the request to storage
+    params(Type): tasks_id(int): Id row to delete
+"""
 def delete_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
     if request.method == 'POST':
         task.delete()
         return redirect('/tasks/')
 
+"""summary: This Method permit edit the task
+    params(Type): request(Object): Body of the request to storage
+    params(Type): tasks_id(int): Id row to edit
+"""
 def edit_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
 
